dc20327d44fabe55a8adb6ec120747619b5f5b18
fabricelifaa <fabfly95@gmail.com>
2024-04-15 11:50:00 +0100

    add recpatcha control

--- controllers/mailler.js
+++ controllers/mailler.js
@@ -1,9 +1,9 @@
 const nodemailer = require("nodemailer");
 const smtpTransport = require("nodemailer-smtp-transport");
-const asyncHandler = require('../middleware/async')
-const ErrorReponse = require('../utils/errorResponse')
-const { sendMailValidation } = require('../utils/validations')
-
+const asyncHandler = require("../middleware/async");
+const ErrorReponse = require("../utils/errorResponse");
+const { sendMailValidation } = require("../utils/validations");
+const axios = require("axios");
 
 const transporter = nodemailer.createTransport(
   smtpTransport({
@@ -14,8 +14,8 @@ const transporter = nodemailer.createTransport(
       pass: process.env.MAILPASS,
     },
     tls: {
-      rejectUnauthorized: process.env.ENV == 'production'? true : false
-  }
+      rejectUnauthorized: process.env.ENV == "production" ? true : false,
+    },
   })
 );
 
@@ -25,22 +25,46 @@ const transporter = nodemailer.createTransport(
  * @access  Private
  */
 exports.sendMail = asyncHandler(async (req, res, next) => {
-  const { from, to, subject, body } = req.body
-  const { error } = sendMailValidation(req.body)
-  if (error) return next(new ErrorReponse(error.details[0].message, 404))
+  const { from, to, subject, body } = req.body;
+  const { error } = sendMailValidation(req.body);
+  if (error) return next(new ErrorReponse(error.details[0].message, 404));
   var mailOptions = {
-    from: from ? from + '<atypikhouse@f2i-dev26-jd.fr>' : 'No-reply <atypikhouse@f2i-dev26-jd.fr>',
+    from: from
+      ? from + "<atypikhouse@f2i-dev26-jd.fr>"
+      : "No-reply <atypikhouse@f2i-dev26-jd.fr>",
     to,
     subject,
     text: body,
   };
-  transporter.sendMail(mailOptions, function(error){
+  transporter.sendMail(mailOptions, function (error) {
     if (error) {
-      console.log(error)
-        res.status(200).json({ success: false, data: null });
+      console.log(error);
+      res.status(200).json({ success: false, data: null });
     } else {
-        res.status(200).json({ success: true, data: null });
+      res.status(200).json({ success: true, data: null });
     }
-  });  
-  
+  });
+});
+
+/**
+ * @desc   send recpatcha token
+ * @route   POST /api/v1/mail/recaptcha/
+ * @access  Private
+ */
+exports.getRecaptcha = asyncHandler(async (req, res, next) => {
+  const { response } = req.params;
+  const verifiedStatus = await axios.get(
+    `https://www.google.com/recaptcha/api/siteverify`,
+    {
+      params: {
+        secret: process.env.RECPATCHA_SK,
+        response: response,
+      },
+      headers: {
+        withCredentials: false,
+        "Access-Control-Allow-Origin": "*",
+      },
+    }
+  );
+  res.status(200).json(verifiedStatus);
 });
--- routes/mailler.js
+++ routes/mailler.js
@@ -3,12 +3,17 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    sendMail
+    sendMail,
+    getRecaptcha
 } = require("../controllers/mailler");
 
 router
   .route("/")
   .post(sendMail)
+
+router
+  .route("/recaptcha/:response")
+  .get(getRecaptcha)
 
   module.exports = router;
 
